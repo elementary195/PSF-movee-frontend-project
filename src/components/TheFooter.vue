ce996e351bed837e5c130d7d9712e88b7b4654fe
Alexey Prokofev <62440186+josqer@users.noreply.github.com>
2021-07-22 12:21:53 +0300

    Footer updated with Vuex.Store

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row justify-content-between text-center text-lg-left">
         <div class="col-6 col-lg-3 mr-auto ml-auto mr-lg-0 ml-lg-0 order-first">
-          <router-link class="navbar-brand text-uppercase mb-4 mr-0" :to="{name: 'Main'}" href="#0">{{ SiteName }}</router-link> <!-- Logotype / Name -->
+          <router-link class="navbar-brand text-uppercase mb-4 mr-0" :to="{name: 'Main'}" href="#0">{{ siteName }}</router-link> <!-- Logotype / Name -->
 
           <a data-toggle="modal" data-target=".bd-example1-modal-lg" class="d-block my-2" href="#" @click.prevent>Политика конфиденциальности</a> <!-- Button trigger modal -->
           <div class="modal fade bd-example1-modal-lg" id="exampleModalScrollable1" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableLabel1" aria-hidden="true">
@@ -56,13 +56,13 @@
         </div> <!-- nav -->
         <div class="col-12 col-lg-auto d-flex flex-column align-items-center my-4 my-lg-0">
           <div class="mb-2">
-            <a :href="sn.link" v-for="sn in SocialNets"><img :src="sn.icon" :alt="sn.name" class="px-2"></a>
+            <a :href="sn.link" v-for="sn in socialNetworks"><img :src="sn.icon" :alt="sn.name" class="px-2"></a>
           </div> <!-- social-nets -->
-          <u class="font-weight-bold">hello@movee.ru</u> <!-- e-mail -->
+          <u class="font-weight-bold">{{ email }}</u> <!-- e-mail -->
         </div> <!-- contacts -->
         <div class="col-12 col-lg-auto d-flex flex-column align-items-center order-last">
           <a href="#order-form" class="btn btn-outline-secondary p-2 mb-lg-2 w-100">Обратный звонок</a>
-          <span class="text-center phone d-none d-lg-block">8(800)-080-12-13</span>
+          <span class="text-center phone d-none d-lg-block">{{ telNum }}</span>
         </div> <!-- phone -->
       </div>
     </div>
@@ -71,14 +71,13 @@
 
 <script>
 import {constants} from "../config";
+import {mapGetters, mapState} from "vuex";
 
 export default {
   name: "Footer",
-  data() {
-    return {
-      SiteName: constants.SITENAME,
-      SocialNets: constants.SOCIALNETWORKS,
-    }
+  computed: {
+    ...mapGetters(['siteName']),
+    ...mapState(['telNum', 'socialNetworks', 'email']),
   }
 }
 </script>
